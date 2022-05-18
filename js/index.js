c1e34dbab1ced016614b526d99fb67b34088f0e1
Funpee <armgod@gmail.com>
2022-05-18 12:40:57 +0800

    Update Electron to latest version
    - 更新到最新版 Electron，大坑
    - title 显示文件名
    - 可拖入新图片替换旧图
    - 🐞 修复了拖动整体时，图片被拖出的问题

--- js/index.js
+++ js/index.js
@@ -1,15 +1,36 @@
-const { remote } = require('electron');
-const { BrowserWindow, Menu, MenuItem, screen } = remote;
+const { BrowserWindow, Menu, MenuItem, screen } = require('@electron/remote')
+// const { remote } = require('electron');
+// const { BrowserWindow, Menu, MenuItem, screen } = remote;
+
+let { ipcRenderer } = require('electron')
+
+
+// 定义基本变量
+var img = document.getElementById("simg");
+let titleBar = document.getElementById("gbtitle");
+
 
 // 重置窗口基本属性
-remote.getCurrentWindow().setAlwaysOnTop(false);
+// remote.getCurrentWindow().setAlwaysOnTop(false);
+rsWindow(420, 320);
 
+// 重置titlebar
+function rsTiltleBar(){
+  titleBar.innerHTML = "Plankton";
+}
 // 改变窗口大小
 function rsWindow(w,h) {
-  remote.getCurrentWindow().setSize(w, h);
-  var app = document.getElementById("app");
-  app.style.width = w + "px";
-  app.style.height = h + "px";
+  newW = w + 0;
+  newH = h + 0;
+  // remote.getCurrentWindow().setSize(newW , newH);
+  ipcRenderer.send('setMainWindow', { "width": w, "height": h })
+}
+// 窗口置顶
+function pinWindow(boolean) {
+  ipcRenderer.send('setAwaysOnTop', boolean );
+}
+function isPinWindow() {
+  ipcRenderer.send('isAwaysOnTop');
 }
 // 清空画布
 function clCanvas() {
@@ -31,24 +52,27 @@ function clCanvas() {
    img.height = h;
    rsWindow(w, h);
  }
-// 改变图片比例
- function scaleImg(n) {
-  var img = document.getElementById("img1").childNodes[0];
-  // var imgbox = document.getElementById("img1");
-  img.width = img.naturalWidth * n;
-  img.height = img.naturalHeight * n;
-  // imgbox.style.transform = "scale(" + n + ")";
+// 改变图片比例，注意是按原始尺寸计算的比例
+
+function scaleImg(n) {
+  let img = document.querySelector("#simg");
+  console.log("执行图片缩放后图片宽高：" + img.width + ", " + img.height)
+  // 将图片修改为自然大小的 n 倍
+  img.setAttribute("style","transform: scale(" + n + ");");
+
+  // 修改窗口大小，如果超出，则显示为指定大小，不再超出屏幕外
   fnWidth = Math.round(img.naturalWidth * n);
   fnHeight = Math.round(img.naturalHeight * n);
   var winWidth = screen.getPrimaryDisplay().workAreaSize.width;
   var winHeight = screen.getPrimaryDisplay().workAreaSize.height;
-   if (fnWidth >= winWidth) {
-     rsWindow(winWidth, winHeight);
+    if (fnWidth >= winWidth) {
+      rsWindow(winWidth, winHeight);
   } else {
-     rsWindow(fnWidth, fnHeight);
+      rsWindow(fnWidth, fnHeight);
   }
-   
- }
+  console.log("========修改图片尺寸完成========")
+}
+
 // 图片拖放功能实现
 let ctInfo = document.getElementById("ctinfo");
 let content = document.getElementsByClassName("content")[0]
@@ -81,84 +105,214 @@ function drop(e) {
 function picture(e) {
   e.preventDefault();//阻止拖拽结束的默认行为，会把文件作为链接打开。
   document.getElementById("app").className = "wrapper showimg";
+  let imgwrapper = document.querySelector("#img1");
   var id = e.target.id;//得到div的id
+  let img = document.querySelector("#simg");
   var box = document.getElementById(id);//通过id得到div
   var file = e.dataTransfer.files[0];//得到文件
   var fileReader = new FileReader();
   fileReader.readAsDataURL(file);//将file读为url
-  fileReader.onload = function (ev) {//为div添加一个图片，图片路径为拖拽的文件路径
-    var img = document.createElement("img");
-    img.src = fileReader.result;
-    box.appendChild(img);
+  fileReader.fileName = file.name;
+  var imgdom = document.createElement("img");
+  if(img == null){
+    fileReader.onload = function (ev) {//为div添加一个图片，图片路径为拖拽的文件路径
+      imgdom.src = fileReader.result;
+      imgdom.setAttribute("id", "simg");
+      imgdom.setAttribute("ondragstart", "'return false;'");
+      imgdom.setAttribute("onselectstart", "'return false;'");
+      box.appendChild(imgdom);
+      // 设定标题栏为文件名
+      let gbFileName = ev.target.fileName;
+      titleBar.innerText = gbFileName;
+      // 加载专有右键菜单
+      contextMenu('imgViewTemplate');
+    }
+  } else {
+    console.log("已有图片")
+    fileReader.onload = function (ev) {
+      img.src = fileReader.result;
+      console.log(img.naturalWidth + " " + img.naturalHeight);
+      // 设定标题栏为文件名
+      let gbFileName = ev.target.fileName;
+      titleBar.innerText = gbFileName;
+    }
+  }
+  
+  console.log("++++已执行 picture 函数++++");
+}
+
+// 给指定 dom 绑定函数
+const imgwrapper = document.querySelector("#img1");
+
+imgwrapper.ondrop = (event) => {
+  picture(event);
+  console.log('图片已添加完毕');
+}
+window.onresize = function() {
+  console.log(window.innerWidth + ", " + window.innerHeight);
+  let img = document.getElementById("simg");
+  // 检查i mg 是否被创建，有则执行相关操作
+  if (img){
+    let wSc = ( window.innerWidth ) / img.width;
+    let hSc = ( window.innerHeight ) / img.height;
+    // 比较这两个比例，谁小就使用谁。以使图片始终适配最短边
+    if (wSc < hSc) {
+      img.setAttribute("style", "transform: translateZ(0) scale(" + wSc + ")");
+      img.setAttribute("datascale", wSc);
+    } else {
+      img.setAttribute("style", "transform: translateZ(0) scale(" + hSc + ")");
+      img.setAttribute("datascale", hSc);
+    }
   }
 }
 
-//右键餐单
-function rightMenu() {
-  const menu = new Menu();
-  menu.append(new MenuItem({
-    label: '50%',
-    type: 'radio',
-    checked: false,
-    click: function () {
-      console.log('50% clicked');
-      scaleImg(0.5);
-      // checked: true
-    }
-  }));
-  menu.append(new MenuItem({
-    label: '100% 原始尺寸',
-    type: 'radio',
-    checked: true,
-    click: function () {
-      console.log('100% clicked');
-      scaleImg(1);
-      // checked: true
-    }
-  }));
-  menu.append(new MenuItem({
-    label: '200%',
-    type: 'radio',
-    checked: false,
-    click: function () {
-      console.log('200% clicked');
-      scaleImg(2);
-      // checked: true
-    }
-  }));
-  menu.append(new MenuItem({ type: 'separator' }));//分割线
-  menu.append(new MenuItem({ label: '放大' }));
-  menu.append(new MenuItem({ label: '缩小' }));
-  menu.append(new MenuItem({ type: 'separator' }));//分割线
-  menu.append(new MenuItem({
-    label: '清空画布',
-    click: function () {
+/*
+======== 全局菜单 ========
+*/
+
+const isMac = process.platform === 'darwin'
+
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { label: '关于', role: 'about' },
+      { type: 'separator' },
+      { label: '服务', role: 'services' },
+      { type: 'separator' },
+      { label: '隐藏', role: 'hide' },
+      { label: '隐藏其它', role: 'hideOthers' },
+      { label: '全部显示', role: 'unhide' },
+      { type: 'separator' },
+      { label: '退出', role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: '文件',
+    submenu: [
+      { 
+        label: '新建窗口', 
+        role: 'new',
+        click: function(){
+
+          // do sth
+
+        }
+      },
+      { type: 'separator' },
+      { label: '打开' },
+      { label: '清空画布' },
+      { type: 'separator' },
+      ...(isMac ? [
+        { label: '关闭', role: 'close' }
+      ]:[
+        { role: 'quit' }
+      ])
+    ]
+  },
+  // { role: '' }
+  {
+    label: '显示',
+    submenu: [
+      { 
+        label: '实际尺寸',
+        accelerator: 'CommandOrControl+0',
+        click: () => {
+          scaleImg(1)
+        }
+    },
+      { label: '放大' },
+      { label: '缩小' },
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: '视图',
+    submenu: [
+      { 
+        label: '清空画布',
+        role: 'reload'
+       },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: '窗口',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      { type: 'separator' },
+      { type: 'separator' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
+  },
+  {
+    label: '帮助',
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+
+/*
+======== 右键菜单 ========
+*/
+// renderer
+function contextMenu(tempName) {
+  window.addEventListener('contextmenu', (e) => {
+    console.log(`开始执行显示菜单命令：${tempName}`)
+    e.preventDefault()
+    ipcRenderer.send('show-context-menu', tempName)
+    console.log(`已执行完毕：${tempName}`)
+  })
+}
+// 这里先注释掉，默认展示一个菜单，但不知道怎么回事，每次都自己运行一次取代了自定义菜单。
+// let mainMenu = contextMenu('mainTemplate')
+// window.onload = mainMenu
+
+// 接收主进程返回的数据
+ipcRenderer.on('context-menu-command', (e, data) => {
+  // ...
+  console.log(`执行以下命令：${data.name}, 数值 ${data.val}`);
+  let command = data.name;
+  let val = data.val;
+  switch (command) {
+    case 'scale-img':
+      scaleImg(val);
+      break;
+    case 'clean-canvas':
       clCanvas();
       console.log("清空已执行");
-      rsWindow(422, 318);
+      rsWindow(420, 320);
       console.log("已重置窗口大小");
-    }
-  }));
-  menu.append(new MenuItem({ type: 'separator' }));//分割线
-  menu.append(new MenuItem({
-    label: '置顶此窗口',
-    type: 'checkbox',
-    click: function () {
-      if (remote.getCurrentWindow().isAlwaysOnTop() == true) {
-        remote.getCurrentWindow().setAlwaysOnTop(false);
-        console.log("// 取消置顶窗口")
-      } else if (remote.getCurrentWindow().isAlwaysOnTop() == false) {
-        remote.getCurrentWindow().setAlwaysOnTop(true);
-        console.log("// 置顶窗口")
-      }
-      
-    }
-  }));
-  window.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
-    menu.popup({ window: remote.getCurrentWindow() })
-  }, false)
-}
+      rsTiltleBar();
+      break;
+    default:
+      console.log(`Sorry, we are out of ${command}.`);
+  }
 
 
-rightMenu();
+})
+
--- main.js
+++ main.js
@@ -1,29 +1,141 @@
-const { app, BrowserWindow, Menu, Tray } = require('electron')
+const { MenuItem } = require('@electron/remote/main')
+const { app, BrowserWindow, Menu, Tray, ipcMain } = require('electron')
+// const path = require('path')
 
-function createWindow() {
+const createWindow = () => {
   // 创建浏览器窗口
   const win = new BrowserWindow({
-    width: 422,
+    width: 420,
     height: 318,
     transparent: true,
     frame: false,
+    roundedCorners: false,
     hasShadow: false,
     webPreferences: {
+      enableRemoteModule: true,
       nodeIntegration: true,
-      enableRemoteModule: true
+      contextIsolation: false,
+      // preload: path.join(__dirname, 'preload.js')
     }
   })
-
+  require('@electron/remote/main').initialize()
+  require("@electron/remote/main").enable(win.webContents)
   // 并且为你的应用加载index.html
   win.loadFile('index.html')
 
   // 打开开发者工具
-  // win.webContents.openDevTools()
-}
+  win.webContents.openDevTools()
+  //接收通知 改变窗口高度
+  const ipc = require('electron').ipcMain;
+  ipc.on("setMainWindow", function (e, data) {
+    win.setSize(data.width, data.height);
+  })
+  ipc.on("setAwaysOnTop", function (e, boolean) {
+    win.setAlwaysOnTop(boolean);
+  })
+  ipc.on("isAwaysOnTop", function (e) {
+    console.log("isAwaysOnTop 收到渲染进程请求");
+    win.isAlwaysOnTop();
+    // mainWindow.webContents.send('is-pin-window', true)
+  })
+  
+  // 测试的右键菜单
+  ipcMain.on('show-context-menu', (event, tempName) => {
+    console.log(`已接收参数：${tempName}`)
+    switch (tempName) {
+      case 'mainTemplate':
+        var template = [
+          {
+            label: '窗口置顶',
+            click: () => {
+            }
+          }
+        ]
+        break;
+      case 'imgViewTemplate':
+        var template = [
+          {
+            label: '放大',
+            click: () => {
+              event.sender.send('context-menu-command', { "name": "scale-img", "val": 1 })
+            }
+          },
+          {
+            label: '缩小',
+            click: () => {
+              event.sender.send('context-menu-command', { "name": "scale-img", "val": .5 })
+            }
+          },
+          {
+            label: '实际大小',
+            click: () => {
+              event.sender.send('context-menu-command', { "name": "scale-img", "val": 1 })
+            }
+          },
+          {
+            label: '固定比例',
+            submenu: [
+            {
+              label: '50%',
+              click: () => {
+                event.sender.send('context-menu-command', { "name": "scale-img", "val": .5 })
+              }
+            },
+            {
+              label: '100% 原始尺寸',
+              click: () => {
+                event.sender.send('context-menu-command', { "name": "scale-img", "val": 1 })
+              }
+            },
+            {
+              label: '200%',
+              click: () => {
+                event.sender.send('context-menu-command', { "name": "scale-img", "val": 2 })
+              }
+            },
+          ]},
+          { type: 'separator' },
+          {
+            label: '清空画布',
+            click: () => {
+              // event.sender.send('context-menu-command', { "name": "clean-canvas" })
+              win.reload()
+            }
+          },
+          { type: 'separator' },
+          {
+            label: '置顶窗口',
+            // 直接召唤主进程窗口置顶方案
+            click: () => {
+              win.setAlwaysOnTop(true)
+              console.log("窗口已置顶");
+            }
+          },
+          {
+            label: '不置顶窗口',
+            // 点击则向渲染进程发送数据
+            click: () => {
+              win.setAlwaysOnTop(false)
+              console.log("取消置顶");
+            }
+          }
+        ]
+        break;
+    }
 
+    
+    const menu = Menu.buildFromTemplate(template)
+    menu.popup(BrowserWindow.fromWebContents(event.sender))
+  })
+}
 // Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
 // 部分 API 在 ready 事件触发后才能使用。
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
